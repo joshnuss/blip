0cebe622582c252b2fcf0d7b3a07bafb80097e2c
Josh Nussbaum <joshnuss@gmail.com>
2018-05-14 22:09:57 -0400

    Adds StatsD.Metric.Counter server

--- lib/statsd.ex
+++ lib/statsd.ex
@@ -1,11 +1,13 @@
 defmodule StatsD do
   def record({:counter, name, count}) do
-    IO.puts "recording stat"
+    {:ok, pid} = find_or_create(name)
+
+    StatsD.Metric.Counter.append(pid, String.to_integer(count))
   end
 
   def record(text) do
     Task.async fn ->
-      case String.split(text, "|") do
+      case text |> String.trim() |> String.split("|") do
         [name] ->
           record({:counter, name, 1})
         [name, "c"] ->
@@ -13,6 +15,16 @@ defmodule StatsD do
         [name, "c", count] ->
           record({:counter, name, count})
       end
+    end
+  end
+
+  def find_or_create(tag) do
+    case Registry.lookup(StatsD.Registry, tag) do
+      [{pid, _}] ->
+        {:ok, pid}
+      _ ->
+        name = {:via, Registry, {StatsD.Registry, tag}}
+        DynamicSupervisor.start_child(StatsD.Metric.Supervisor, {StatsD.Metric.Counter, name})
     end
   end
 end
---
+++ lib/statsd/metric/counter.ex
@@ -0,0 +1,34 @@
+defmodule StatsD.Metric.Counter do
+  use GenServer
+
+  @period 1000
+
+  def start_link(name={:via, Registry, {StatsD.Registry, tag}}) do
+    state = %{
+      tag: tag,
+      sum: 0,
+    }
+
+    GenServer.start_link(__MODULE__, state, name: name)
+  end
+
+  def init(state) do
+    :timer.send_interval(@period, :flush)
+
+    {:ok, state}
+  end
+
+  def handle_cast(n, state) do
+    {:noreply, %{state | sum: state.sum + n}}
+  end
+
+  def handle_info(:flush, state) do
+    StatsD.Backend.persist({state.tag, :counter, state.sum})
+
+    {:noreply, %{state | sum: 0}}
+  end
+
+  def append(tag, value) do
+    GenServer.cast(tag, value)
+  end
+end
