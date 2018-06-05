9528fb4b8e5b264dfc451a50fee421c7a8aa1ff6
Josh Nussbaum <joshnuss@gmail.com>
2018-06-04 20:02:31 -0400

    Adds help and version number flags

--- lib/statsd/cli.ex
+++ lib/statsd/cli.ex
@@ -7,6 +7,27 @@ defmodule StatsD.CLI do
     OptionParser.parse(args)
   end
 
+  defp start(options = {[version: true], _, _}) do
+    {:ok, vsn} = :application.get_key(:statsd, :vsn)
+
+    IO.puts("v#{vsn}")
+  end
+
+  defp start(options = {[help: true], _, _}) do
+    IO.puts """
+    USAGE: statsd <portA> <portB> ... <portN> [options]
+
+    Default port is 2052
+
+    OPTIONS
+
+      --help
+      --version
+      --flush default is `1000`
+      --backend default is `console`
+    """
+  end
+
   defp start(options = {_, port, _}) do
     set_port(port)
 
